3cc2cf32047501c7e6caff0c9a8c3c26184d372d
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2023-11-07 17:31:09 -0500

    completed phase0

--- Board.rb
+++ Board.rb
@@ -4,7 +4,10 @@ class Board
     def initialize
         @rows= Array.new(8){Array.new(8,nil)}
         Piece.new
-
+        @rows[0].map! {|el| el = Piece.new}
+        @rows[1].map! {|el| el = Piece.new}
+        @rows[6].map! {|el| el = Piece.new}
+        @rows[7].map! {|el| el = Piece.new}
     end
 
     def populate_piece(pos)
@@ -23,8 +26,17 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
+        piece = self[start_pos]
 
+        raise "error" if start_pos.nil?
 
+        if (end_pos.all? {|el| el >= 0 && el < 8}) && self[end_pos].nil?
+            self[end_pos] = piece
+        else
+            raise "not valid"
+        end
+
+        self[start_pos] = nil 
     end
 
 end
